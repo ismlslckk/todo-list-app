4271ac28010640842109e25cb235c1a4af51a8a1
ismlslckk <ismlslck@gmail.com>
2023-05-08 00:21:17 +0200

    login page designed is done

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,43 @@
-const Login = () => (
+import { useState } from 'react';
+import classNames from 'classnames';
+import { InnerWrapper, Wrapper } from '@/components/atoms';
+import styles from './login.module.scss';
 
-  <>Login page</>
-);
+const Form = () => {
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+  });
 
-export default Login;
+  const handleChange = (e:any) => {
+    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    setForm((state) => ({
+      ...state,
+      [e.target.name]: value,
+    }));
+  };
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Wrapper>
+      <InnerWrapper>
+        <div className={classNames(styles.wrapper, styles.fadeInDown)}>
+          <div id="formContent">
+            <h2 className={styles.active}> Sign In </h2>
+
+            <form onSubmit={handleSubmit}>
+              <input type="text" value={form.username} onChange={handleChange} id="login" name="username" className={classNames(styles.fadeIn, styles.second)} placeholder="username" />
+              <input type="password" value={form.password} onChange={handleChange} id="password" name="password" className={classNames(styles.fadeIn, styles.third)} placeholder="password" />
+              <input type="submit" className={classNames(styles.fadeIn, styles.fourth)} value="Log In" />
+            </form>
+          </div>
+        </div>
+      </InnerWrapper>
+    </Wrapper>
+  );
+};
+
+export default Form;
