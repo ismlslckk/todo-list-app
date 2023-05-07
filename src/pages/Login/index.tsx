d16442869a5d5375e0db18798899216bf529509d
ismlslckk <ismlslck@gmail.com>
2023-05-08 00:32:29 +0200

    login page is rollback

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
-import classNames from 'classnames';
 import { InnerWrapper, Wrapper } from '@/components/atoms';
-import styles from './login.module.scss';
 
-const Form = () => {
-  const [form, setForm] = useState({
+const Form = () =>
+/*   const [form, setForm] = useState({
     username: '',
     password: '',
   });
@@ -19,25 +16,14 @@ const Form = () => {
 
   const handleSubmit = (e:any) => {
     e.preventDefault();
-  };
+  }; */
 
-  return (
+  // eslint-disable-next-line implicit-arrow-linebreak
+  (
     <Wrapper>
       <InnerWrapper>
-        <div className={classNames(styles.wrapper, styles.fadeInDown)}>
-          <div id="formContent">
-            <h2 className={styles.active}> Sign In </h2>
-
-            <form onSubmit={handleSubmit}>
-              <input type="text" value={form.username} onChange={handleChange} id="login" name="username" className={classNames(styles.fadeIn, styles.second)} placeholder="username" />
-              <input type="password" value={form.password} onChange={handleChange} id="password" name="password" className={classNames(styles.fadeIn, styles.third)} placeholder="password" />
-              <input type="submit" className={classNames(styles.fadeIn, styles.fourth)} value="Log In" />
-            </form>
-          </div>
-        </div>
+        login page . .
       </InnerWrapper>
     </Wrapper>
   );
-};
-
 export default Form;
