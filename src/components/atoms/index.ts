f21091683a5fe1124dd9986d0bae9a1f4370e6de
ismlslckk <ismlslck@gmail.com>
2023-05-12 15:46:58 +0200

    initial commit

--- src/components/atoms/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Wrapper } from './Wrapper';
-export { default as InnerWrapper } from './InnerWrapper';
-export { default as Button } from './Button';
-export { default as Input } from './Input';
